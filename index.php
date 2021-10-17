e759a3c82f76e5f09ee15fedd335b45c96900ea8
Nikolai312-lab <nikint478@gmail.com>
2021-10-17 13:10:04 +0300

    Modified traffic lights

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
 
 $roadMap = [
-  ['distance'=> "23", 'time'=>"23"],
-  ['distance'=> "37", 'time'=>"30" ]
+  'distance'=> "24", 'time'=>"7",
+  'distance'=> "31", 'time'=>"15",
+  'distance'=> "50", 'time'=>"41",
 ];
 
-
-#function timePassed($roadMap){
-#  while ($roadMap[0]['time'] <= $roadMap[0]['distance']) {
-#    $roadMap[0]['time'] += $roadMap[0]['time'];
-#  }
+#while ($roadMap[0]['time'] <= $roadMap[0]['distance']) {
+#  $roadMap[0]['time'] += $roadMap[0]['time'];
 #}
+#while ($roadMap[1]['time'] <= $roadMap[1]['distance']) {
+#  $roadMap[1]['time'] += $roadMap[1]['time'];
+#}
+#echo "You will reach the end of the first stotlight at: " . $roadMap[0]['time'] ."<br> ";
+#echo "You'll reach the end of the second stotligh at: " . $roadMap[1]['time'];
+foreach ($roadMap as $distance) {
+  while ( $roadMap['time']<= $roadMap['distance']) {
 
-while ($roadMap[0]['time'] <= $roadMap[0]['distance']) {
-  $roadMap[0]['time'] += $roadMap[0]['time'];
+    $roadMap['time'] += $roadMap['time'];
+
+  }
+
 }
-while ($roadMap[1]['time'] <= $roadMap[1]['distance']) {
-  $roadMap[1]['time'] += $roadMap[1]['time'];
-}
-echo "You will reach the end of the first stotlight at: " . $roadMap[0]['time'] ."<br> ";
-echo "You'll reach the end of the second stotligh at: " . $roadMap[1]['time'];
-#echo timePassed();
+echo "In that much time you are going to pass the last traffic light:  " . $roadMap['time'] . " seconds";
