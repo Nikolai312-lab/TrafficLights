79d1a85491095e1622b82f4a7c3450fd19429bd4
Nikolai312-lab <nikint478@gmail.com>
2021-10-19 10:17:07 +0300

    This should be working now

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
 
 $roadMap = [
-  'distance'=> "1", 'time'=>"2",
-  'distance'=> "3", 'time'=>"2"
+  'distance'=> "9", 'time'=>"10",
+  'distance'=> "13", 'time'=>"14",
+  #'distance'=> "167", 'time'=>"35",
+  #'distance'=> "211", 'time'=>"11",
+  #'distance'=> "289", 'time'=>"46"
 ];
 
-foreach ($roadMap as $time) {
-  while ($roadMap['time'] <= $roadMap['distance'] ) {
-    $roadMap['time'] += $roadMap['time'];
-  }
-}
-
-echo "In that much time you are going to pass the last traffic light:  " . $roadMap['time'] . " seconds";
+if ($roadMap['distance'] < $roadMap['time']) {
+      $roadMap['time'] --;
+      echo "In that much time you are going to pass the last traffic light:  " . $roadMap['time'] . " seconds";
+    }
+    else {
+      while ($roadMap['time'] <= $roadMap['distance']) {
+           $roadMap['time'] += $roadMap['time'];
+        }
+       echo "In that much time you are going to pass the last traffic light:  " . $roadMap['time'] . " seconds";
+    }
